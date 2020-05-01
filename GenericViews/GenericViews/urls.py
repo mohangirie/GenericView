36cd9cc769cbb9eeb9a98cd4f3de506582b7e8ed
mohangirie <mohangirie@gmail.com>
2020-05-01 14:56:23 +0530

    modified make entry and add make entry url

--- GenericViews/GenericViews/urls.py
+++ GenericViews/GenericViews/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from GenericApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.makeentry)
 ]
